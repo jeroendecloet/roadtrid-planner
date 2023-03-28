af9118c5781f948c3d6c2dd02d94919dd682775e
Jeroen de Cloet <jeroen.decloet@gmail.com>
2023-03-28 23:12:17 +0200

    Changed JSON structure and add_markers a bit

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import folium
 
 
 class MapItems:
+    """
+    Class to easily get and set items in the JSON configuration file.
+    """
 
     def __init__(self, d=None, filename=None):
         if d is None:
@@ -58,14 +61,16 @@ class MapItems:
 
 
 class Locator:
-
+    """
+    Class for finding coordinates of locations.
+    """
     def __init__(self):
         self.locator = Nominatim(user_agent="GetLoc")
 
     def __call__(self, *args, **kwargs):
         return self.get_coordinates(*args, **kwargs)
 
-    def get_coordinates(self, location: str, additional_info: str) -> list[Union[float, None]]:
+    def get_coordinates(self, location: str, additional_info: str = "") -> list[Union[float, None]]:
         """ Gets the latitude and longitude of a location. Additional information can be supplied, such as a country."""
         _loc_query = ' '.join([location, additional_info])
         print(f"Getting location for {_loc_query}...")
@@ -79,7 +84,9 @@ class Locator:
 
 
 class MapMaker:
-
+    """
+    Class to construct the map with all its components.
+    """
     def __init__(self, map_item_filename=None):
 
         if map_item_filename is not None:
@@ -94,83 +101,71 @@ class MapMaker:
     def create_map(self) -> None:
         self.base_map = folium.Map(location=self.mi['main']['coordinates'], control_scale=True, zoom_start=7)
 
-    @staticmethod
-    def create_icon(icon) -> folium.Icon:
-        if 'hotel' in icon:
-            return folium.Icon(color="green", icon="bed", prefix='fa')
-        elif icon == 'location':
-            return folium.Icon(color='blue', icon='location-dot', prefix='fa')
-        elif 'landmark' in icon:
-            return folium.Icon(color='red', icon='landmark', prefix='fa')
-        elif icon == 'volcano':
-            return folium.Icon(color='red', icon='volcano', prefix='fa')
-        elif icon == 'waterfall':
-            # _waterfall = folium.features.CustomIcon("icons/waterfall3.png", icon_size=(30, 40))
-            return folium.Icon(color='red', icon="water", prefix='fa')
-        elif icon == 'tree':
-            return folium.Icon(color='red', icon='tree', prefix='fa')
-        elif icon == "food":
-            return folium.Icon(color='blue', icon='utensils', prefix='fa')
-        else:
-            # Try to make icon
-            return folium.Icon(color='red', icon=icon, prefix='fa')
-
     def _add_coordinates(self, keys: Union[list, str]) -> None:
         """ Adds coordinates to locations that do not have coordinates yet. """
         if isinstance(keys, str):
             keys = [keys]
 
         _added_coordinates = False
-        for key in keys:
-            assert key in self.mi.d.keys(), f"Key {key} not in MapItems!"
-            for loc in self.mi[key]:
-                if ("coordinates" not in self.mi[[key, loc]]) or (not self.mi[[key, loc, "coordinates"]]):
-                    if "country" in self.mi["main"]:
-                        self.mi[[key, loc]]["coordinates"] = self.locator(loc, self.mi[["main", "country"]])
-                    else:
-                        self.mi[[key, loc]]["coordinates"] = self.locator(loc)
-                    _added_coordinates = True
+        for loc in self.mi[keys]:
+            if ("coordinates" not in self.mi[keys + [loc]]) or (not self.mi[keys + [loc, "coordinates"]]):
+                if "country" in self.mi["main"]:
+                    self.mi[keys + [loc, "coordinates"]] = self.locator(loc, self.mi[["main", "country"]])
+                else:
+                    self.mi[keys + [loc, "coordinates"]] = self.locator(loc)
+                _added_coordinates = True
 
         if _added_coordinates:
             # If any coordinates have been added; save the MapItems to JSON
             self.mi.to_json()
 
-    def add_markers(self, locations: dict[str, dict], icon: str) -> None:
-        for loc, info_dict in locations.items():
-            assert "coordinates" in info_dict.keys(), f"{loc} is missing coordinates!"
-            # if icon == "landmarks":
-            #     # popup = folium.Popup(" ".join([loc, info_dict['info']]), parse_html=True)
+    @staticmethod
+    def _create_icon(icon: str, color: str) -> folium.Icon:
+        if 'hotel' in icon:
+            return folium.Icon(color=color, icon="bed", prefix='fa')
+        elif 'landmark' in icon:
+            return folium.Icon(color=color, icon='landmark', prefix='fa')
+        elif icon == "food":
+            return folium.Icon(color=color, icon='utensils', prefix='fa')
+        else:
+            # Try to make icon
+            return folium.Icon(color=color, icon=icon, prefix='fa')
 
-            _info = f"{info_dict['info']}<br>" if 'info' in info_dict else ''
-            _website = f"""<a href="{info_dict['website']}" target="_blank">website</a><br>""" if 'website' in info_dict else ''
-            _price = f"""Prijs: &euro; {info_dict['prijs']}<br> """ if 'prijs' in info_dict else ''
-            _availability = "Beschikbaarheid: <ul>{availability}</ul>".format(availability=''.join([f"<li>{item}</li>" for item in info_dict['beschikbaarheid']])) if 'beschikbaarheid' in info_dict else ''
-            html = """<b>{loc}</b><br>{website}{price}{info}{availability}
-            """.format(
-                loc=loc,
-                website=_website,
-                price=_price,
-                info=_info,
-                availability=_availability
-            )
-            popup = folium.Popup(folium.IFrame(html, width=200, height=200))
+    def add_markers(self) -> None:
+        markers = self.mi["markers"]
+        for item, locations in markers.items():
+            self._add_coordinates(["markers", item])
+            for loc, info_dict in locations.items():
+                assert "coordinates" in info_dict.keys(), f"{loc} is missing coordinates!"
 
-            # elif icon == "hotels":
-            #     beschikbaarheid = ''.join([f"<li>{item}</li>" for item in info_dict['beschikbaarheid']])
-            #     html = f"""<b>{loc}</b><br>
-            #         Prijs: &euro; {info_dict['prijs']}<br>
-            #         <a href="{info_dict['website']}" target="_blank">website</a><br>
-            #         Beschikbaarheid: <ul>{beschikbaarheid}</ul>
-            #         """
-            #     popup = folium.Popup(folium.IFrame(html, width=200, height=200))
-            # else:
-            #     raise ValueError
+                _info = f"{info_dict['info']}<br>" if 'info' in info_dict else ''
+                _website = f"""<a href="{info_dict['website']}" target="_blank">website</a><br>""" if 'website' in info_dict else ''
+                _price = f"""Prijs: &euro; {info_dict['prijs']}<br> """ if 'prijs' in info_dict else ''
+                _availability = "Beschikbaarheid: <ul>{availability}</ul>".format(availability=''.join([f"<li>{item}</li>" for item in info_dict['beschikbaarheid']])) if 'beschikbaarheid' in info_dict else ''
 
-            folium.Marker(
-                location=info_dict["coordinates"],
-                popup=popup,
-                icon=self.create_icon(info_dict['icon'] if 'icon' in info_dict.keys() else icon)
-            ).add_to(self.base_map)
+                html = """<b>{loc}</b><br>{website}{price}{info}{availability}""".format(
+                    loc=loc,
+                    website=_website,
+                    price=_price,
+                    info=_info,
+                    availability=_availability
+                )
+                popup = folium.Popup(folium.IFrame(html, width=200, height=200))
+
+                colors = {
+                    "hotel": "green",
+                    "landmark": "red",
+                    "food": "blue"
+                }
+
+                folium.Marker(
+                    location=info_dict["coordinates"],
+                    popup=popup,
+                    icon=self._create_icon(
+                        icon=info_dict['icon'] if 'icon' in info_dict.keys() else item,
+                        color=colors[item]
+                    )
+                ).add_to(self.base_map)
 
     def save_map(self, filename: str) -> None:
         self.base_map.save(filename)
@@ -178,10 +173,7 @@ class MapMaker:
     def main(self) -> None:
         self.create_map()
 
-        items = ['landmarks', 'hotels', "food"]
-        for item in items:
-            self._add_coordinates([item])
-            self.add_markers(self.mi[item], icon=item)
+        self.add_markers()
 
 
 if __name__ == "__main__":
